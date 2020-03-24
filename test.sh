1f446c9433cdc936f0b1e447d2a0f2210095838e
Daniel Augustin <hossie@hossie.de>
2020-03-24 17:55:17 +0100

    bla

--- test.sh
+++ test.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-echo foo
-sudo whoami
 AUTOBUILD_FILE="$(curl -s http://gentoo.osuosl.org/releases/amd64/autobuilds/latest-stage4-amd64-minimal.txt | tail -n1 | awk '{print $1}')"
 AUTOBUILD_DL="http://gentoo.osuosl.org/releases/amd64/autobuilds/${AUTOBUILD_FILE}"
 AUTOBUILD_FILENAME="$(echo $AUTOBUILD_FILE | xargs basename)"
 TEMPDIR="$(mktemp -d)"
 pwd
+env
 cd "${TEMPDIR}"
 wget -nv "${AUTOBUILD_DL}"
 mkdir mnt
