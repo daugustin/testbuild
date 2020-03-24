8c6f203c8b888ac6609ea635fcfa10be38b03902
Daniel Augustin <hossie@hossie.de>
2020-03-24 17:35:22 +0100

    bla

--- test.sh
+++ test.sh
@@ -7,3 +7,15 @@ AUTOBUILD_FILENAME="$(echo $AUTOBUILD_FILE | xargs basename)"
 TEMPDIR="$(mktemp -d)"
 cd "${TEMPDIR}"
 wget -nv "${AUTOBUILD_DL}"
+mkdir mnt
+fallocate -l 10G image
+DEVICE=$(losetup -f --show image)
+parted -s "${DEVICE}" mklabel gpt
+parted -s --align=none "${DEVICE}" mkpart bios_boot 0 2M
+parted -s --align=none "${DEVICE}" mkpart primary 2M 100%
+parted -s "${DEVICE}" set 1 boot on
+parted -s "${DEVICE}" set 1 bios_grub on
+mkfs.ext4 -m0 "${DEVICE}"p2
+e2label "${DEVICE}"p2 cloudimg-rootfs
+mount "${DEVICE}"p2 mnt
+tar --xattrs -xpf "${AUTOBUILD_FILENAME}" -C mnt
